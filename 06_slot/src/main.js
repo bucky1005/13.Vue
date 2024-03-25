aafcd83e800a7e68368a772072fb5aa5e8267941
LAPTOP-CQOG6PAO\Playdata <null00007@naver.com>
2024-03-25 16:12:05 +0900

    프로젝트 모듈화 실습
    vue, js, html로 나누어 import하여 사용

--- 06_slot/src/main.js
+++ 06_slot/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue'         // App.vue에서 script 부분에 export로 작성한 것들을 import해서 App으로 정의하여 사용
 
-createApp(App).mount('#app')
+createApp(App).mount('#app')        // #app은 index.html에 있음
